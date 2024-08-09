29ba559ba262c4047506da8697db1ef3c2ee4218
isimsek7 <isimsekmusic@gmail.com>
2024-08-09 16:21:46 +0300

    Add files via upload

---
+++ DefaultMachine.cs
@@ -0,0 +1,66 @@
+﻿using System;
+namespace OopEndProject
+{
+	public abstract class DefaultMachine
+	{
+		public DefaultMachine()
+		{
+			string _uretimTarihi = DateTime.Now.ToLongDateString();
+			Console.WriteLine("Uretim tarihi="+_uretimTarihi);
+		}
+		public int _seriNo { get; set; }
+		public string _ad { get; set; }
+		public string _aciklama { get; set; }
+		public string _isletimSistemi { get; set; }
+		public virtual void Ozet()
+		{
+		}
+
+
+    }
+	public class Telefon : DefaultMachine
+	{
+		public bool _lisans { get; set; }
+
+        public override void Ozet()
+        {
+			if(_lisans)
+            Console.WriteLine($"TR'de kullanim lisansina sahip, {_seriNo}'seri numarali,{_isletimSistemi}'isletim sistemli {_ad} telefonu. Detaylar={_aciklama}");
+			else
+            Console.WriteLine($"TR'de kullanim lisansina sahip olmayan, {_seriNo}'seri numarali,{_isletimSistemi}'isletim sistemli {_ad} telefonu. Detaylar={_aciklama}");
+
+        }
+    }
+	public class Bilgisayar : DefaultMachine
+	{
+		int _usbGirisSayisi;
+
+		public int UsbGirisSayisi
+		{
+			get
+			{
+				return _usbGirisSayisi;
+			}
+			set
+			{
+				if (value != 2 || value != 4)
+				{
+					Console.WriteLine("Gecersiz giris sayisi verildi.");
+					_usbGirisSayisi = -1;
+				}
+				else
+					_usbGirisSayisi = value;
+			}
+
+		}
+
+		public bool _bluetooth { get; set; }
+        public override void Ozet()
+        {
+			if(_bluetooth)
+            Console.WriteLine($"{_seriNo}'seri numarali,{_isletimSistemi}'isletim sistemli,{_usbGirisSayisi}'usb girisli ve bluetooth'lu {_ad} bilgisayari. Detaylar={_aciklama}");
+            else
+            Console.WriteLine($"{_seriNo}'seri numarali,{_isletimSistemi}'isletim sistemli,{_usbGirisSayisi}'usb girisli {_ad} bilgisayari. Detaylar={_aciklama} \n (Bluetooth=Yok)");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using OopEndProject;
+bool tekrar = true;
+while (tekrar)
+{
+    Console.Write("Telefon icin 1, Bilgisayar icin 2'yi tuslayiniz. =");
+    int userInput = Convert.ToInt32(Console.ReadLine());
+    if (userInput == 1)
+    {
+        Telefon telefon1 = new Telefon();
+        Console.WriteLine("Lutfen telefonun bilgilerini giriniz.");
+        Console.Write("Seri Numarasi= ");
+        telefon1._seriNo = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Ismi= ");
+        telefon1._ad = Console.ReadLine();
+        Console.Write("Isletim sistemi= ");
+        telefon1._isletimSistemi = Console.ReadLine();
+        Console.Write("Cihaz lisansa sahip mi ? (Evet,Hayir)= ");
+        string lisans = Console.ReadLine().ToLower().Trim();
+        if (lisans == "hayir")
+            telefon1._lisans = false;
+        else if (lisans == "evet")
+            telefon1._lisans = true;
+        Console.WriteLine("Lutfen cihaz hakkinda bir aciklama giriniz.");
+        telefon1._aciklama = Console.ReadLine();
+        telefon1.Ozet();
+    }
+    else if (userInput == 2)
+    {
+        Bilgisayar pc = new Bilgisayar();
+        Console.WriteLine("Lutfen bilgisayarin bilgilerini giriniz.");
+        Console.Write("Seri Numarasi= ");
+        pc._seriNo = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Ismi= ");
+        pc._ad = Console.ReadLine();
+        Console.Write("Isletim sistemi= ");
+        pc._isletimSistemi = Console.ReadLine();
+        Console.Write("Cihazin kac usb girisi var= ");
+        pc.UsbGirisSayisi = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Bluetooth var mi (Evet,Hayir)= ");
+        string bluetooth = Console.ReadLine().ToLower().Trim();
+        if (bluetooth == "hayir")
+            pc._bluetooth = false;
+        else if (bluetooth == "evet")
+            pc._bluetooth = true;
+        Console.WriteLine("Lutfen cihaz hakkinda bir aciklama giriniz.");
+        pc._aciklama = Console.ReadLine();
+        pc.Ozet();
+    }
+
+    Console.Write("Yeni bir urun kaydetmek ister misiniz ? (Evet/Hayir)= ");
+    string goOrNo = Console.ReadLine().ToLower().Trim();
+    if (goOrNo == "hayir")
+        tekrar = false;
+    else if (goOrNo == "evet")
+        continue;
+    else
+    {
+        Console.WriteLine("Kullanimi olmayan bir giris yapildi.");
+    }
+}
